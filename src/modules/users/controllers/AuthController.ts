e01851cca3e6c8654924c98fbe05caabb53df267
Henrique Daltrozo <henriquedaltrozo3@gmail.com>
2024-07-08 13:43:30 -0300

    refactor: fix auth controller

--- src/modules/users/controllers/AuthController.ts
+++ src/modules/users/controllers/AuthController.ts
@@ -9,19 +9,15 @@ class AuthController {
     try {
       const { email, password } = req.body;
 
-      console.log('AuthController - email:', email, 'password:', password); // Adicionando log
-
       const user = await User.findOne({ email });
 
       if (!user) {
-        console.log('AuthController - User not found'); // Adicionando log
         throw new Error('Incorrect email/password combination');
       }
 
       const passwordMatched = await bcrypt.compare(password, user.password);
 
       if (!passwordMatched) {
-        console.log('AuthController - Password does not match'); // Adicionando log
         throw new Error('Incorrect email/password combination');
       }
 
@@ -29,19 +25,15 @@ class AuthController {
         { id: user.id },
         process.env.JWT_SECRET as string,
         {
-          expiresIn: '1h',
+          expiresIn: '12h',
         },
       );
-
-      console.log('AuthController - Token generated:', token); // Adicionando log
 
       return res.status(200).json({ token });
     } catch (error) {
       if (error instanceof Error) {
-        console.log('AuthController - Error:', error.message); // Adicionando log
         return handleError(res, error);
       }
-      console.log('AuthController - Unknown Error'); // Adicionando log para erro desconhecido
       return res.status(500).json({ message: 'Unknown error occurred' });
     }
   }
